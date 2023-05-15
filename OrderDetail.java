7a37621360982119a0a75c00c0de6a01f05434a5
Truongthong55 <125253373+Truongthong55@users.noreply.github.com>
2023-05-15 18:56:27 +0700

    OrderDetail.java

---
+++ OrderDetail.java
@@ -0,0 +1,36 @@
+package THT3;
+
+public class OrderDetail {
+    private int quantity;
+    private Product product;
+
+    public OrderDetail(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public double calcTotalPrice() {
+        return this.quantity * this.product.getPrice();
+    }
+
+    @Override
+    public String toString() {
+        return "OrderDetail [quantity=" + quantity + ", product=" + product + "]";
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}
